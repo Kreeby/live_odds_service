f60fe5e1a07e2029e3a4d025fcad1eafa507ae48
Rashad Naghiyev <rashad.naghiyev@gmail.com>
2023-08-02 01:50:17 +0200

    Removed the main class as it's library

--- src/main/java/com/coding/live_odds_service/LiveOddsServiceApplication.java
+++
@@ -1,7 +0,0 @@
-package com.coding.live_odds_service;
-
-public class LiveOddsServiceApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
